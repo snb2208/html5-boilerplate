e57f6709cbbe105c3a599f0635967dbf6201b3b7
snb2208 <SNB@Sindhuras-MacBook-Air.local>
2017-05-08 18:56:17 +0200

    menu bar

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,29 @@
 console.log('Hello world!');
 
 
+
+$( ".hamburger" ).click(function() {
+  $( this ).toggleClass( "change" );
+  var isOpen = $(this).hasClass('change');
+  console.log(isOpen)
+  if (isOpen) {
+  	$('nav.menu').show();
+  } else {
+  	$('nav.menu').hide();
+  }
+});
+
+
+$('x').click(function(){
+	x.hide();
+});
+
+
 $(document).ready(function() {
 	console.log('ready');
 	var headerHeight = null;
 	var docHeight = null;
-
+	
 });
 
 $(window).resize(function() {
