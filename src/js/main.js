b86d8b34e82bddbcad364adf3ed6a7539610d62e
snb2208 <SNB@Sindhuras-MacBook-Air.local>
2017-05-19 06:13:00 +0200

    final responsive full site

--- src/js/main.js
+++ src/js/main.js
@@ -1,53 +1,51 @@
-console.log('Hello world!');
 
-var isMenuOpen = false;
-
-//Hamburger menu animate to close button
-$( ".hamburger" ).click(function() {
-  $( this ).toggleClass( "change" );
-  var isOpen = $(this).hasClass('change');
-  console.log(isOpen)
-  if (isOpen) {
-  	$('nav.menu').show();
-  	$('body').css({'overflow': 'hidden'});
-  } else {
-  	$('nav.menu').hide();
-  	$('body').css({'overflow': 'scroll'});
-  }
-});
-
-$('nav ul li').click(function(){
-	$('body').css({'overflow': 'scroll'});
-});
-
-
-
-
-//Message display on hover over images
-$(".anime-image-wrapper img").hover(hoverIn, hoverOut);
-
-function hoverIn(){
-	var hoveredId = $(this).attr('id');
-	var messageClass = '.message-' + hoveredId;
-	$(messageClass).show();
-}
-
-function hoverOut(){
-	var hoveredId = $(this).attr('id');
-	var messageClass = '.message-' + hoveredId;
-	$(messageClass).hide();
-}
-
-$(".menu").click(function(){
-	$(".menu").hide();
-	$(".hamburger").toggleClass("change");
-
-});
 
 
 
 $(document).ready(function() {
 	console.log('ready');
+
+	//Hamburger menu animate to close button
+	$( ".hamburger" ).click(function() {
+	  $( this ).toggleClass( "change" );
+	  var isOpen = $(this).hasClass('change');
+	  console.log(isOpen)
+	  if (isOpen) {
+	  	$('nav.menu').show();
+	  	$('body').css({'overflow': 'hidden'});
+	  } else {
+	  	$('nav.menu').hide();
+	  	$('body').css({'overflow': 'scroll'});
+	  }
+	});
+
+	//To prevent scrolling when menu is open
+	$('nav ul li').click(function(){
+		$('body').css({'overflow': 'scroll'});
+	});
+
+
+
+//Message display on hover over images in Home page
+	$(".anime-image-wrapper img").hover(hoverIn, hoverOut);
+
+	function hoverIn(){
+		var hoveredId = $(this).attr('id');
+		var messageClass = '.message-' + hoveredId;
+		$(messageClass).show();
+	}
+
+	function hoverOut(){
+		var hoveredId = $(this).attr('id');
+		var messageClass = '.message-' + hoveredId;
+		$(messageClass).hide();
+	}
+
+	$(".menu").click(function(){
+		$(".menu").hide();
+		$(".hamburger").toggleClass("change");
+
+	});
 	
 });	
 
